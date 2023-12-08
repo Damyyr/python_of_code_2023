bb8d57766d69ac1b21161c84f6dcef3ce4016c9a
tchansii <cbrouard.01@gmail.com>
2023-12-07 23:31:15 -0500

    Finished part 4 (probably the first todo i've ever done in my life)

--- 4/tchansii.py
+++ 4/tchansii.py
@@ -12,21 +12,21 @@ def part1():
         print("Part 1 : " + str(total_points))
 
 def part2():
-    print("Part 2 : Not Done 🥲")
-    # TODO: finish this
-    # inventory = {}
-    # card_counter = 1 # i guess its a counter, could have named this deck_index or somth idk
-    # with open('input.txt', 'r') as file:
-    #     for game in file:
-    #         parsed_game = game.split(": ", 1)[1].strip().split("|")
-    #         winning_numbers = list(map(int, parsed_game[0].split()))
-    #         numbers_obtained = list(map(int, parsed_game[1].split()))
-    #         matches = len(set(winning_numbers) & set(numbers_obtained))
-    #         next = [i for i in range(card_counter + 1, card_counter + matches + 1)]
-    #         card_counter += 1
-    #         print(next)
-
-
+    with open('input.txt', 'r') as file:
+        games = [game.split(": ", 1)[1].strip().split("|") for game in file]
+        game_counts = [1] * len(games)
+        total_cards = len(games)
+        i = 0
+        while i < total_cards:
+            winning_numbers = list(map(int, games[i][0].split()))
+            numbers_obtained = list(map(int, games[i][1].split()))
+            matches = len(set(winning_numbers) & set(numbers_obtained))
+            if matches > 0 and i < total_cards - 1:
+                for j in range(i+1, min(i+1+matches, total_cards)):
+                    game_counts[j] += game_counts[i]
+            i += 1
+        total_cards = sum(game_counts)
+        print("Part 2 : " + str(total_cards))
 
 part1()
 part2()
